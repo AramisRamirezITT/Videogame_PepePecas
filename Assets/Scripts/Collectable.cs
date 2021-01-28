9a08aec137e64d0c2b766b25dd1aa870048052b3
Aramiskkck69 <jlaramis9@hotmail.com>
2021-01-28 09:55:10 -0700

    Before Menu

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour
+{
+    public static int cornCollectable = 0;
+    public ParticleSystem collectaParticleSystem;
+    public AudioSource collectableAudio;
+    public Text CollectableText;
+    
+    void Start()
+    {
+        cornCollectable = 0;
+        collectaParticleSystem = GameObject.Find("ParticlesCollectable").GetComponent<ParticleSystem>();
+        collectableAudio = GetComponentInParent<AudioSource>();
+        CollectableText = GameObject.Find("CollectableContador (number)").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            collectaParticleSystem.transform.position = transform.position; // las particulas se colocan en donde estan los elotes
+            collectaParticleSystem.Play();
+            collectableAudio.Play();
+            gameObject.SetActive(false);
+            cornCollectable++;
+            CollectableText.text = cornCollectable.ToString();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    int hp = 3;
+    public Image[] hearts;
+    bool hasCooldown = false;
+
+    public SceneChanger changeScene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (GetComponent<PlayerMove>().isGrounded)
+            {
+                SubstractHealth();
+            }
+        }
+    }
+
+    void SubstractHealth()
+    {
+        if (!hasCooldown)
+        {
+            if(hp > 0)
+            {
+                hp--;
+                hasCooldown = true;
+
+                StartCoroutine(Cooldown());
+            }
+
+            if(hp <= 0)
+            {
+                changeScene.ChangeSceneTo("LoseScene");
+            }
+
+            EmptyHearts();
+        }
+    }
+
+    void EmptyHearts()
+    {
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if (hp - 1 < i)
+                hearts[i].gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(.5f);
+        hasCooldown = false;
+
+        StopCoroutine(Cooldown());
+    }
+    
+}
